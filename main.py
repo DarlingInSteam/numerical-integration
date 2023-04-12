93ed9486050cf600048b2b21b7d156abe6ec1eaa
DarlingPc <rombk2711@gmail.com>
2023-04-12 15:03:04 +0700

    add runge rule

--- main.py
+++ main.py
@@ -27,6 +27,35 @@ def rectangle_method(left, right, func, eps=0.001):
     print(second_split, steps)
 
 
+def rectangle_method_with_runge(left, right, func, eps=0.001):
+
+    def divide_sum(number_of_splits):
+        nonlocal left, right, func
+        step = (right - left) / float(number_of_splits)
+        sum_in_step = sum([func(left + k * step) for k in range(0, number_of_splits)])
+        result_in_step = step * sum_in_step
+        return result_in_step
+
+    steps = 2
+    first_split = divide_sum(steps)
+    steps *= 2
+    second_split = divide_sum(steps)
+
+    while abs(first_split - second_split) > eps:
+        steps *= 2
+        first_split = divide_sum(steps)
+        steps *= 2
+        second_split = divide_sum(steps)
+
+        # Runge rule
+        runge_sum = divide_sum(steps * 2)
+        error = abs(runge_sum - second_split) / 3
+        if error > eps:
+            steps *= 2
+
+    print(second_split, steps)
+
+
 def trapezoidal_rule(left, right, func, eps=0.001):
     n = 1
     prev_result = 0
@@ -74,6 +103,53 @@ def simpson_method(left, right, function, epsilon):
     print(tmp_sum, step+1)
 
 
+def simpson_with_runge(left, right, function, epsilon):
+    n = 2
+    h = (right - left) / (2 * n)
+
+    prev_sum = 0
+    tmp_sum = float(function(left)) + \
+              float(function(right))
+
+    while abs(tmp_sum - prev_sum) > epsilon:
+        prev_sum = tmp_sum
+        tmp_sum = float(function(left)) + \
+                  float(function(right))
+
+        for step in range(1, 2 * n):
+            if step % 2 != 0:
+                tmp_sum += 4 * float(function(left + step * h))
+            else:
+                tmp_sum += 2 * float(function(left + step * h))
+
+        tmp_sum *= h / 3
+        n *= 2
+        h /= 2
+
+        # Используем правило Рунге для контроля точности
+        runge_sum = float(function(left)) + \
+                    float(function(right))
+
+        for step in range(1, 2 * n, 2):
+            runge_sum += 4 * float(function(left + step * h))
+
+        for step in range(2, 2 * n - 1, 2):
+            runge_sum += 2 * float(function(left + step * h))
+
+        runge_sum *= h / 3
+
+        error = abs(runge_sum - tmp_sum) / 15
+        if error > epsilon:
+            n *= 2
+            h /= 2
+
+    print(tmp_sum, step+2)
+
+
 rectangle_method(5, 10, f, 0.000001)
+rectangle_method_with_runge(5, 10, f, 0.000001)
+print('')
 trapezoidal_rule(5, 10, f, 0.000001)
-simpson_method(5, 10, f,   0.000001)
+print('')
+simpson_method(5, 10, f,   0.00001)
+simpson_with_runge(5, 10, f, 0.00001)
