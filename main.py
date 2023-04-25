ddcf3bfb4448ec149f9020818104877d1d78f5a4
darlingPc <rombk2711@gmail.com>
2023-04-25 23:25:18 +0700

    fix runge rule in methods

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def rectangle_method(left, right, func, eps=0.001):
     def divide_sum(number_of_splits):
         nonlocal left, right, func
         step = (right - left) / float(number_of_splits)
-        sum_in_step = sum([func(left + k * step) for k in range(0, number_of_splits)])
+        sum_in_step = sum([func(left + k * step + step/2) for k in range(0, number_of_splits)])
         result_in_step = step * sum_in_step
         return result_in_step
 
@@ -28,11 +28,10 @@ def rectangle_method(left, right, func, eps=0.001):
 
 
 def rectangle_method_with_runge(left, right, func, eps=0.001):
-
     def divide_sum(number_of_splits):
         nonlocal left, right, func
         step = (right - left) / float(number_of_splits)
-        sum_in_step = sum([func(left + k * step) for k in range(0, number_of_splits)])
+        sum_in_step = sum([func(left + k * step + step/2) for k in range(0, number_of_splits)])
         result_in_step = step * sum_in_step
         return result_in_step
 
@@ -54,27 +53,6 @@ def rectangle_method_with_runge(left, right, func, eps=0.001):
             steps *= 2
 
     print(second_split, steps)
-
-
-def trapezoidal_rule(left, right, func, eps=0.001):
-    n = 1
-    prev_result = 0
-    result = (right - left) * (func(left) + func(right)) / 2
-
-    while abs(result - prev_result) > eps:
-        n *= 2
-        h = (right - left) / n
-        x = left + h
-        sum = 0
-        for i in range(1, n):
-            sum += func(x)
-            x += h
-        sum *= 2
-        sum += (func(left) + func(right))
-        prev_result = result
-        result = (h * sum) / 2
-
-    print(result, n)
 
 
 def simpson_method(left, right, function, epsilon):
@@ -103,10 +81,10 @@ def simpson_method(left, right, function, epsilon):
     print(tmp_sum, step+1)
 
 
-def simpson_with_runge(left, right, function, epsilon):
+def simpson_method_with_runge(left, right, function, epsilon):
     n = 2
     h = (right - left) / (2 * n)
-
+    step = 0
     prev_sum = 0
     tmp_sum = float(function(left)) + \
               float(function(right))
@@ -123,33 +101,84 @@ def simpson_with_runge(left, right, function, epsilon):
                 tmp_sum += 2 * float(function(left + step * h))
 
         tmp_sum *= h / 3
-        n *= 2
-        h /= 2
 
-        # Используем правило Рунге для контроля точности
-        runge_sum = float(function(left)) + \
-                    float(function(right))
+        # используем метод Рунге для уточнения результата
+        double_n = 2 * n
+        double_h = h / 2
+        double_sum = float(function(left)) + \
+                     float(function(right))
 
-        for step in range(1, 2 * n, 2):
-            runge_sum += 4 * float(function(left + step * h))
+        for step in range(1, 2 * double_n):
+            if step % 2 != 0:
+                double_sum += 4 * float(function(left + step * double_h))
+            else:
+                double_sum += 2 * float(function(left + step * double_h))
 
-        for step in range(2, 2 * n - 1, 2):
-            runge_sum += 2 * float(function(left + step * h))
+        double_sum *= double_h / 3
+        error = abs(tmp_sum - double_sum) / 15
 
-        runge_sum *= h / 3
-
-        error = abs(runge_sum - tmp_sum) / 15
         if error > epsilon:
             n *= 2
             h /= 2
 
-    print(tmp_sum, step+2)
+    print(tmp_sum, step)
+
+
+def trapezoidal_rule(left, right, func, eps=0.001):
+    n = 1
+    prev_result = 0
+    result = (right - left) * (func(left) + func(right)) / 2
+
+    while abs(result - prev_result) > eps:
+        n *= 2
+        h = (right - left) / n
+        x = left + h
+        sum = 0
+        for i in range(1, n):
+            sum += func(x)
+            x += h
+        sum *= 2
+        sum += (func(left) + func(right))
+        prev_result = result
+        result = (h * sum) / 2
+
+    print(result, n)
+
+
+def trapezoidal_rule_with_runge(left, right, func, eps=0.001):
+    n = 1
+    prev_result = 0
+    result = (right - left) * (func(left) + func(right)) / 2
+
+    while abs(result - prev_result) > eps:
+        n *= 2
+        h = (right - left) / n
+        x = left + h
+        sum = 0
+        for i in range(1, n):
+            sum += func(x)
+            x += h
+        sum *= 2
+        sum += (func(left) + func(right))
+        prev_result = result
+        result = (h * sum) / 2
+        
+        # оценка погрешности по методу Рунге
+        error = abs(result - prev_result) / 3
+        
+        # уменьшение шага, если погрешность больше заданной
+        if error > eps:
+            n *= 2
+            h /= 2
+
+    print(result, n)
 
 
 rectangle_method(5, 10, f, 0.000001)
 rectangle_method_with_runge(5, 10, f, 0.000001)
 print('')
 trapezoidal_rule(5, 10, f, 0.000001)
+trapezoidal_rule_with_runge(5, 10, f, 0.000001)
 print('')
-simpson_method(5, 10, f,   0.00001)
-simpson_with_runge(5, 10, f, 0.00001)
+simpson_method(5, 10, f, 0.0000001)
+simpson_method_with_runge(5, 10, f, 0.0000001)
